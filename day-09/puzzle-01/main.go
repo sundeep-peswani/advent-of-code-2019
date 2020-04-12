34065467250be0dff8a5d2da3c78d1da34634182
Sundeep Peswani <sundeep@sundeeppeswani.com>
2020-04-12 10:45:11 -0400

    Day 9: Complete

---
+++ day-09/puzzle-01/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/sundeep-peswani/advent-of-code-2019/intcode"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		os.Exit(1)
+	}
+
+	filename := os.Args[1]
+	input, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	in, out := make(chan int, 1), make(chan int, 1)
+	system := intcode.NewIntcode(in, out)
+	system.Load(string(input))
+
+	go system.Run()
+	in <- 1
+
+	fmt.Println(<-out)
+}
